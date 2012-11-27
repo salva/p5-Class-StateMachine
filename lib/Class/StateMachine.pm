76b14f51de750b88a7ed09ddc94679197e02478f
Salvador <sfandino@yahoo.com>
2012-11-27 18:36:42 +0100

    resolve method names before manipulating it as a bug on pure-perl version of Package::Stash may otherwise clobber them

--- lib/Class/StateMachine.pm
+++ lib/Class/StateMachine.pm
@@ -180,9 +180,11 @@ EOE
 }
 
 sub _move_state_methods {
+
+    $_[1] //= CvGV($_[2] for @state_methods
+
     while (@state_methods) {
 	my ($class, $sym, $sub, @on_state) = @{shift @state_methods};
-	$sym //= CvGV($sub);
 	my ($method) = $sym=~/([^:]+)$/ or croak "invalid symbol name '$sym'";
 
         my $stash = Package::Stash->new($class);
